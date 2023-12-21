ae17a470482d3e68a6153439d719adb78cdbe4d8
Proxxa <yorip625@gmail.com>
2023-12-21 17:11:10 -0500

    formatting

--- src/day_twenty.rs
+++ src/day_twenty.rs
@@ -1,4 +1,10 @@
-use std::{io::{Cursor, Read}, process::Command, fs::File, path::PathBuf, ffi::OsStr};
+use std::{
+    ffi::OsStr,
+    fs::File,
+    io::{Cursor, Read},
+    path::PathBuf,
+    process::Command,
+};
 
 use log::private::error;
 use rocket::{http::Status, serde::json::Json, *};
@@ -17,9 +23,7 @@ pub fn archive_files(data: Vec<u8>) -> Result<Json<usize>, (Status, String)> {
 }
 
 #[post("/archive_files_size", data = "<data>")]
-pub fn archive_files_size(
-    data: Vec<u8>,
-) -> Result<Json<u64>, (Status, String)> {
+pub fn archive_files_size(data: Vec<u8>) -> Result<Json<u64>, (Status, String)> {
     tar::Archive::new(Cursor::new(data))
         .entries()
         .and_then(|mut e| e.try_fold(0u64, |size, b| b.map(|b| size + b.size())))
@@ -37,7 +41,11 @@ pub async fn cookie(data: Vec<u8>) -> Result<String, (Status, String)> {
             error!("Got to the big scary block");
 
             let output = Command::new("git")
-                .args(["log", "--format=%cn,%H", "christmas",/* "--", "santa.txt"*/])
+                .args([
+                    "log",
+                    "--format=%cn,%H",
+                    "christmas", /* "--", "santa.txt"*/
+                ])
                 .current_dir(tempdir.path())
                 .output()
                 .map_err(map_err::<500>)?;
@@ -45,11 +53,15 @@ pub async fn cookie(data: Vec<u8>) -> Result<String, (Status, String)> {
             info!("Output:\n\n{output}\n\n");
 
             let output = Command::new("git")
-                .args(["log", "--format=%cn,%H", "christmas", /*"--", "santa.txt"*/])
+                .args([
+                    "log",
+                    "--format=%cn,%H",
+                    "christmas", /*"--", "santa.txt"*/
+                ])
                 .current_dir(tempdir.path())
                 .output()
                 .map_err(map_err::<500>)?;
-            
+
             // The only error here should occur if `christmas` is not a real branch. (:
             if !output.status.success() {
                 error!("Returning 400!!");
@@ -58,11 +70,13 @@ pub async fn cookie(data: Vec<u8>) -> Result<String, (Status, String)> {
                 return dbg!(Err(err_string).map_err(map_err::<400>));
             }
 
-
             // This isn't a good idea.
             // I'm doing it anyway. I will regret it.
-            for (author, commit) in String::from_utf8(output.stdout).map_err(map_err::<500>)?.lines().map(|l| l.split_once(",").unwrap()) {
-
+            for (author, commit) in String::from_utf8(output.stdout)
+                .map_err(map_err::<500>)?
+                .lines()
+                .map(|l| l.split_once(",").unwrap())
+            {
                 let output = Command::new("git")
                     .args(["checkout", commit, "--force"])
                     .current_dir(tempdir.path())
@@ -71,19 +85,23 @@ pub async fn cookie(data: Vec<u8>) -> Result<String, (Status, String)> {
 
                 // If this fails, I've failed horribly somewhere.
                 let fail_string = String::from_utf8(output.stderr).map_err(map_err::<500>)?;
-                assert!(output.status.success(), "This is a very big problem.\n{fail_string}\n");
-
-
+                assert!(
+                    output.status.success(),
+                    "This is a very big problem.\n{fail_string}\n"
+                );
 
                 let mut checstr = String::new();
                 let Ok(path) = find_santa(tempdir.path().to_owned()) else { continue };
 
-                if !path.exists() { 
+                if !path.exists() {
                     error!("Doesn't exist for {author} {commit} despite getting to this point");
-                    continue
+                    continue;
                 }
 
-                File::open(path).map_err(map_err::<501>)?.read_to_string(&mut checstr).map_err(map_err::<502>)?;
+                File::open(path)
+                    .map_err(map_err::<501>)?
+                    .read_to_string(&mut checstr)
+                    .map_err(map_err::<502>)?;
 
                 error!("{author} {commit}\n{checstr}");
 
@@ -91,20 +109,26 @@ pub async fn cookie(data: Vec<u8>) -> Result<String, (Status, String)> {
                     return dbg!(Ok(format!("{author} {commit}")));
                 }
             }
-            
+
             Err(dbg!("Could not find santa's cookie.")).map_err(map_err::<400>)
         })
-        .map(|a| { drop(tempdir); dbg!(a) })
+        .map(|a| {
+            drop(tempdir);
+            dbg!(a)
+        })
         .map_err(|e| dbg!(e))
 }
 
-
 fn find_santa(pat: PathBuf) -> Result<PathBuf, ()> {
-
     if pat.is_file() {
         info!("Hit file: {}", pat.display())
     }
-    if pat.is_file() && pat.file_name().expect("Dear god why does this not have a name") == OsStr::new("santa.txt") {
+    if pat.is_file()
+        && pat
+            .file_name()
+            .expect("Dear god why does this not have a name")
+            == OsStr::new("santa.txt")
+    {
         return Ok(pat);
     }
 
@@ -118,6 +142,6 @@ fn find_santa(pat: PathBuf) -> Result<PathBuf, ()> {
             }
         }
     }
-    
+
     Err(())
 }
